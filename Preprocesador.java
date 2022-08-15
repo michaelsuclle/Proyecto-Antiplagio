9b6489929b415ede1200dbbf0bfe0dc97d2e4b60
benjaminsuclle <msuclles@unsa.edu.pe>
2022-08-15 02:30:46 -0500

    ya esta implementado el arbol

--- Preprocesador.java
+++ Preprocesador.java
@@ -1,46 +1,46 @@
 import java.util.ArrayList;
 public class Preprocesador {
 	static String textooriginal;//texto que es 100% original 
-	private ArrayList<ArbolBST> arreglodearboles= new ArrayList<ArbolBST>();//en cada arbol hay una oración
+	static ArrayList<ArbolBST> arreglodearboles= new ArrayList<ArbolBST>();//en cada arbol hay una oración
 	public Preprocesador(){
 	}
 
 	public Preprocesador(String textooriginal){
+		this.textooriginal=textooriginal;
 		iniciarPreprocesamiento();
 	}
 
 	public static void main(String[] args){
-
 		Preprocesador obj = new Preprocesador("Había una vez un conejito soñador que vivía en una casita en medio del bosque, rodeado de libros y fantasía, pero no tenía amigos. Todos le habían dado de lado porque se pasaba el día contando historias imaginarias sobre hazañas caballerescas, aventuras submarinas y expediciones extraterrestres. Siempre estaba inventando aventuras como si las hubiera vivido de verdad, hasta que sus amigos se cansaron de escucharle y acabó quedándose solo.\nAl principio el conejito se sintió muy triste y empezó a pensar que sus historias eran muy aburridas y por eso nadie las quería escuchar. Pero pese a eso continuó escribiendo.\nLas historias del conejito eran increíbles y le permitían vivir todo tipo de aventuras. Se imaginaba vestido de caballero salvando a inocentes princesas o sintiendo el frío del mar sobre su traje de buzo mientras exploraba las profundidades del océano.");
-
 	}
 
 	static void iniciarPreprocesamiento(){
 		//System.out.println(textooriginal);
 
-		ArrayList<String> oraciontemporal = new ArrayList<String>();
 		String palabratemporal = "";
 		int contadordecalidad = 0;
+		ArbolBST arboltemporal = new ArbolBST();
 		for (int i=0; i<textooriginal.length(); i++){
 			String caractertemporal = textooriginal.substring(i,i+1);
-			if(caractertemporal.equals(" ")||caractertemporal.equals(";")||caractertemporal.equals(":")){ //aqui termina una palabra, hemos generalizado un poco esto
+			if(caractertemporal.equals(" ")||caractertemporal.equals(";")||caractertemporal.equals(":")||caractertemporal.equals("\n")){ //aqui termina una palabra, hemos generalizado un poco esto
 				if(contadordecalidad >3){
-					oraciontemporal.add(palabratemporal);
-					System.out.println(palabratemporal + " - es una palabra util");				}
+					System.out.println(palabratemporal + " - es una palabra util");				
+					arboltemporal.insertar(new Node(palabratemporal));//aqui estamos generando el arbol, palabra por palabra(ademas estamos creando un nodo por cada palabra)
+				}
+
 				palabratemporal = "";
 				contadordecalidad = 0;
 				continue;
 
 			}else if(caractertemporal.equals(".")){
-				oraciontemporal.add(palabratemporal);
 
 				System.out.println("\naqui termino una oración\n");
 
 
-				//enviamos oración temporal a la funcion que generara el arbol
-				//
-				//
-				oraciontemporal = new ArrayList<String>();
+				//enviamos el arbol temporal a el arreglo global
+				arreglodearboles.add(arboltemporal);
+				arboltemporal = new ArbolBST();
+
 				palabratemporal = "";
 				contadordecalidad = 0;
 			}else{
